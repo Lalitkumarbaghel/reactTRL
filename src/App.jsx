dead68a962025c622383bfa2ea828b057ee038fb
Lalit Kumar <77280738+Lalitkumarbaghel@users.noreply.github.com>
2025-02-18 15:04:57 +0530

    add new componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Header from "./componets/Header";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>React js step by step</h1>
-      <p>Code with Lalit  baghel</p>
-    </>
-  )
+    <div>
+      <Header/>
+      <h1>Hello React js, what are doing this time</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <h1>React js step by step</h1>
+//       <p>Code with Lalit  baghel</p>
+//     </>
+//   )
+// }
+
+// export default App
